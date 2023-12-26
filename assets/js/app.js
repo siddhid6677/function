7d513173bbaaa1bd5576f2411ee897b18a73f21d
siddhid6677 <150324418+siddhid6677@users.noreply.github.com>
2023-12-26 15:04:24 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,185 @@
+var cl = console.log;
+
+// Functions >> means the relation between input and output.
+
+function addOf2nums(x,y){
+	cl(x + y)
+}
+
+addOf2nums(100,200);
+
+addOf2nums(1000,2000);
+
+addOf2nums(10,20);
+
+addOf2nums(150,120);
+
+addOf2nums(500,700);
+
+function addOf3nums(a,b,c){
+	cl( a + b + c)
+}
+
+addOf3nums(10,20,30);
+
+addOf3nums(40,50,60);
+
+addOf3nums(70,80,90);
+
+addOf3nums(1000,2000,3000);
+
+addOf3nums(399,499,599);
+
+function substraOf2nums(x,y){
+	cl(x - y)
+}
+
+substraOf2nums(500,1000);
+
+substraOf2nums(999,599);
+
+substraOf2nums(90,30);
+
+substraOf2nums(150,120);
+
+substraOf2nums(15,5);
+
+function multiOf2nums(x,y){
+	cl(x * y)
+}
+
+multiOf2nums(100,200);
+
+multiOf2nums(499,699);
+
+multiOf2nums(1240,260);
+
+multiOf2nums(1234,4567);
+
+multiOf2nums(500,90);
+
+function diviOf2nums(x,y){
+	cl(x / y)
+}
+
+diviOf2nums(50000,30);
+
+diviOf2nums(999,299);
+
+diviOf2nums(45000,1290);
+
+diviOf2nums(2000,100);
+
+diviOf2nums(90000,70);
+
+var x;
+cl(x, typeof x);
+
+function printName(){
+	cl('Bond, James Bond!!!!')
+}
+printName()
+
+
+function addOf2nums(x,y){
+	var result = x + y;
+	return result;
+}
+
+var add1 = addOf2nums(500,1000);
+cl(add1);
+
+function addOf2nums(x,y){
+	var result = x + y;
+	return result;
+}
+
+var add2 = addOf2nums(5000,1000);
+cl(add2);
+
+function addOf2nums(x,y){
+	var result = x + y;
+	return result;
+}
+
+var add3 = addOf2nums(999,9900);
+cl(add3);
+
+
+
+function substraOf2nums(x,y){
+	var result = x - y;
+	return result;
+}
+
+var substra1 = substraOf2nums(500,1000);
+cl(substra1);
+
+
+function substraOf2nums(x,y){
+	var result = x - y;
+	return result;
+}
+
+var substra2 = substraOf2nums(999,599);
+cl(substra2);
+
+
+function substraOf2nums(x,y){
+	var result = x - y;
+	return result;
+}
+
+var substra3 = substraOf2nums(90,30);
+cl(substra3);
+
+
+function multiOf2nums(x,y){
+	var result = x * y;
+	return result;
+}
+
+var multi1 = multiOf2nums(100,200);
+cl(multi1);
+
+function multiOf2nums(x,y){
+	var result = x * y;
+	return result;
+}
+
+var multi2 = multiOf2nums(499,699);
+cl(multi2);
+
+function multiOf2nums(x,y){
+	var result = x * y;
+	return result;
+}
+
+var multi3 = multiOf2nums(1240,260);
+cl(multi3);
+
+
+function diviOf2nums(x,y){
+	var result = x / y;
+	return result;
+}
+
+var divi1 = diviOf2nums(50000,30);
+cl(divi1);
+
+function diviOf2nums(x,y){
+	var result = x / y;
+	return result;
+}
+
+var divi2 = diviOf2nums(2000,100);
+cl(divi2);
+
+function diviOf2nums(x,y){
+	var result = x / y;
+	return result;
+}
+
+var divi3 = diviOf2nums(45000,1290);
+cl(divi3);
+
